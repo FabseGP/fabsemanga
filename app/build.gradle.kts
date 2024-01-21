ba3e1c4283b7e134dcf3d2e4d5a30c1978bf01bd
fabseman <fabse_utilities@pm.me>
2024-01-21 13:43:18 +0100

    Current version 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     defaultConfig {
         applicationId = "app.fabsemanga"
 
-        versionCode = 3
-        versionName = "0.1.1"
+        versionCode = 2
+        versionName = "0.1.0"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
